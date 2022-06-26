0f040c39fcde5adcf8b648f8a43af9a3addcc578
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-06-26 17:48:42 +0530

    Time: 92 ms, Memory: 14.9 MB - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,5 +1,35 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        pos = self.partition(nums, 0, len(nums)-1)
+        if pos > len(nums) - k:
+            return self.findKthLargest(nums[:pos], k-(len(nums)-pos))
+        elif pos < len(nums) - k:
+            return self.findKthLargest(nums[pos+1:], k)
+        else:
+            return nums[pos]
+
+        
+    def partition(self, nums, l, r):
+        index = random.randint(0, len(nums) - 1)
+        nums[index], nums[r] = nums[r], nums[index]
+        pivot = nums[r]
+        lo = l 
+        for i in range(l, r):
+            if nums[i] < pivot:
+                nums[i], nums[lo] = nums[lo], nums[i]
+                lo += 1
+        nums[lo], nums[r] = nums[r], nums[lo]
+        return lo
+        
+        
+        
+        
+        
+        
+        
+        
+    def naive(nums,k):
+        ''' O(klogk)+O((n-k) log (n-k))'''
         import heapq
         res=[]
         for i in range(len(nums)):
