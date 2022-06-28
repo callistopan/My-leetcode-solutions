4b90b7ca5ac4b68f7310cc56531270118d170b5f
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-06-28 17:17:54 +0530

    Time: 541 ms (8.70%), Space: 14.9 MB (51.90%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -3,6 +3,36 @@ class Solution:
         frequency=[0]*26
         for char in s:
             frequency[ord(char)-ord('a')]+=1
+        frequency.sort(reverse=True)
+        delete_count=0
+        
+        max_freq_allowed=len(s) # maximum frequency allowed
+        
+        for freq in frequency:
+            
+            if freq >max_freq_allowed:
+                delete_count+= freq - max_freq_allowed
+                freq=max_freq_allowed
+            
+            max_freq_allowed=max(0,freq-1)
+            
+        return delete_count
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
+    def minDeletion2(s):
+        
+        '''time complexity== O(N+K^2) K is the maximum possible number of distinct characters'''
+        frequency=[0]*26
+        for char in s:
+            frequency[ord(char)-ord('a')]+=1
         delete_count=0
         seen_frequencies=set()
         
