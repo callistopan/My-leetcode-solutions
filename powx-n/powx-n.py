9ec04cf3a97d41d34e6863cc7f6fda24bc61729c
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-06-28 12:42:33 +0530

    Time: 67 ms, Memory: 13.9 MB - LeetHub

--- powx-n/powx-n.py
+++ powx-n/powx-n.py
@@ -1,5 +1,7 @@
 class Solution:
     def myPow(self, x: float, y: int) -> float:
+        '''if the least significant bit of y is 0, the result is (x^y/2)^2 otherwise, it is x*(x^y/2)^2.
+'''
         result, power = 1.0, y
         if y < 0:
             power, x = -power, 1.0/x
