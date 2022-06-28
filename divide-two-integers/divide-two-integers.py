397206d45fffabc04d532260a15b3f44c7556403
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-06-28 12:32:39 +0530

    Time: 37 ms, Memory: 13.9 MB - LeetHub

--- divide-two-integers/divide-two-integers.py
+++ divide-two-integers/divide-two-integers.py
@@ -2,8 +2,14 @@ class Solution:
     def divide(self, A: int, B: int) -> int:
         if (A == -2147483648 and B == -1): return 2147483647
         a, b, res = abs(A), abs(B), 0
-        for x in range(32)[::-1]:
-            if (a >> x) - b >= 0:
-                res += 1 << x
-                a -= b << x
+        power=32
+        b_power=b<<power
+        while a>=b:
+            while b_power >a:
+                b_power>>=1
+                power-=1
+            res+=1<<power
+            a-=b_power
+        
+                
         return res if (A > 0) == (B > 0) else -res
