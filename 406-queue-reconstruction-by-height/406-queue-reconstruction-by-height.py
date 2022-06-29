9ed5c7ee4e560ab4f961eb9e669335ff96a4f444
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-06-29 09:10:15 +0530

    Time: 1791 ms (5.06%), Space: 14.7 MB (14.52%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -6,7 +6,7 @@ class Solution:
         least_front_most=sorted(people,key= lambda x:x[1])
        
             
-        seen={}
+        seen=set()
         look_up={}
         for i in range(len(people)):
             h,k=least_front_most[i]
@@ -20,10 +20,9 @@ class Solution:
             f=0
             for i in range(len(least_front_most)):
                 if f==0 and count==k and (least_front_most[i][0],least_front_most[i][1]) not in seen:
-                    least_front_most[index],least_front_most[i]=least_front_most[i],least_front_most[index]
-                   
+                    least_front_most[index],least_front_most[i]=least_front_most[i],least_front_most[index]           
                     look_up[(least_front_most[index][0],least_front_most[index][1])]=index
-                    seen[(h,k)]=True
+                    seen.add((h,k))
                     f=1
                     
                 if count<k and least_front_most[i][0]>=h:
