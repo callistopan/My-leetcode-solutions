a09f7f5235e6d3c415088c553b7087fc834b418c
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-06-29 09:11:12 +0530

    Time: 723 ms (13.09%), Space: 14.7 MB (6.40%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -17,13 +17,13 @@ class Solution:
             h,k=people
             index=look_up[(h,k)]
             count=0
-            f=0
+
             for i in range(len(least_front_most)):
-                if f==0 and count==k and (least_front_most[i][0],least_front_most[i][1]) not in seen:
+                if count==k and (least_front_most[i][0],least_front_most[i][1]) not in seen:
                     least_front_most[index],least_front_most[i]=least_front_most[i],least_front_most[index]           
                     look_up[(least_front_most[index][0],least_front_most[index][1])]=index
                     seen.add((h,k))
-                    f=1
+                    break
                     
                 if count<k and least_front_most[i][0]>=h:
                     count+=1
