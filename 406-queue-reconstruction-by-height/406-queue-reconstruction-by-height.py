6c78520dcda08fdea91288d2f99abd91688e6964
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-06-29 08:40:41 +0530

    Time: 947 ms (8.31%), Space: 14.6 MB (14.52%) - LeetHub

---
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -0,0 +1,31 @@
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        
+        people_sorted=sorted(people,key= lambda x:x[0])
+            
+        least_front_most=sorted(people,key= lambda x:x[1])
+       
+            
+        seen={}
+        look_up={}
+        for i in range(len(people)):
+            h,k=least_front_most[i]
+            look_up[(h,k)]=i
+            
+            
+        for people in people_sorted:
+            h,k=people
+            index=look_up[(h,k)]
+            count=0
+            f=0
+            for i in range(len(least_front_most)):
+                if f==0 and count==k and (least_front_most[i][0],least_front_most[i][1]) not in seen:
+                    least_front_most[index],least_front_most[i]=least_front_most[i],least_front_most[index]
+                    look_up[(least_front_most[i][0],least_front_most[i][1])]=i
+                    look_up[(least_front_most[index][0],least_front_most[index][1])]=index
+                    seen[(h,k)]=True
+                    f=1
+                    
+                if count<k and least_front_most[i][0]>=h:
+                    count+=1
+        return least_front_most
