f5c7c3f82d5bd1253c7a733553faae355fec52ac
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-06-30 17:39:17 +0530

    Time: 65 ms (47.33%), Space: 13.8 MB (74.82%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.py
@@ -0,0 +1,34 @@
+class Solution:
+    def nextPermutation(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        inversion_point= len(arr)-2
+        while inversion_point>=0 and arr[inversion_point]>=arr[inversion_point+1]:
+            inversion_point-=1
+
+
+        if inversion_point==-1:
+            arr.sort()
+            return
+
+        # swap the smallest entry after the inversion point
+        # that is greater than the inversion point
+        # use binary search
+        # find the smallest entry after the inversion point
+
+        for i in range(len(arr)-1,inversion_point,-1):
+            if arr[i]>arr[inversion_point]:
+                arr[i],arr[inversion_point]=arr[inversion_point],arr[i]
+                break
+
+        # reverse the array after the inversion point
+        l=inversion_point+1
+        r=len(arr)-1
+        while l<r:
+            arr[l],arr[r]=arr[r],arr[l]
+            l+=1
+            r-=1
+
+        
