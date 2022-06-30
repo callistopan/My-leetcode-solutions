447a8b960dbc98c9cdbd9bcd8ffd174ae7bbac3d
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-06-30 08:27:27 +0530

    Time: 2104 ms (43.81%), Space: 34.6 MB (26.33%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_total_profit=0
+        min_price_so_far=float("inf")
+        
+        first_buy=[0]*len(prices)
+        
+        # forward
+        
+        for i ,price in enumerate(prices):
+            min_price_so_far=min(min_price_so_far,price)
+            max_total_profit=max(max_total_profit,price-min_price_so_far)
+            first_buy[i]=max_total_profit
+            
+            
+        # backward phase maximum profit if we make a second by on that day
+        
+        max_price_so_far=float('-inf')
+        
+        for i ,price in reversed(list(enumerate(prices[1:],1))):
+            max_price_so_far=max(max_price_so_far,price)
+            
+            max_total_profit=max(max_total_profit,max_price_so_far-price+first_buy[i-1])
+        return max_total_profit
+        
+        
