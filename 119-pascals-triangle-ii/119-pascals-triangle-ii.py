c186e441f4bbf4ba7d487d71d3d8698431b3577c
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-07-01 11:09:46 +0530

    Time: 51 ms (38.94%), Space: 13.9 MB (15.87%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        
+        '''  using O(n) space'''
+        
+        pascal = [1]*(rowIndex + 1)
+        
+        print(pascal)
+        
+        for i in range(2,rowIndex+1):
+            
+            for j in range(i-1,0,-1):
+                
+                pascal[j] += pascal[j-1]  
+                
+            print(pascal)
+                
+        return pascal
