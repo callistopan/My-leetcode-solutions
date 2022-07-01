a601e621f67dd1a1f87efd13e6b897b4fb61259a
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-07-01 15:53:07 +0530

    Time: 675 ms, Memory: 61.4 MB - LeetHub

--- insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,48 +1,58 @@
-import random
 class RandomizedSet:
 
     def __init__(self):
-        self.s=set()
-        """
-        Initialize your data structure here.
-        """
-        
+        self.data_map = {} # dictionary, aka map, aka hashtable, aka hashmap
+        self.data = [] # list aka array
 
     def insert(self, val: int) -> bool:
-        
 
-        if val not in self.s:
-            self.s.add(val)
-            return True
-        else:
+        # the problem indicates we need to return False if the item 
+        # is already in the RandomizedSet---checking if it's in the
+        # dictionary is on average O(1) where as
+        # checking the array is on average O(n)
+        if val in self.data_map:
             return False
-        """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        """
+
+        # add the element to the dictionary. Setting the value as the 
+        # length of the list will accurately point to the index of the 
+        # new element. (len(some_list) is equal to the index of the last item +1)
+        self.data_map[val] = len(self.data)
+
+        # add to the list
+        self.data.append(val)
         
+        return True
 
     def remove(self, val: int) -> bool:
-        if val in self.s:
-            self.s.remove(val)
-            return True
-        else:
+
+        # again, if the item is not in the data_map, return False. 
+        # we check the dictionary instead of the list due to lookup complexity
+        if not val in self.data_map:
             return False
-        """
-        Removes a value from the set. Returns true if the set contained the specified element.
-        """
-        
+
+        # essentially, we're going to move the last element in the list 
+        # into the location of the element we want to remove. 
+        # this is a significantly more efficient operation than the obvious 
+        # solution of removing the item and shifting the values of every item 
+        # in the dicitionary to match their new position in the list
+        last_elem_in_list = self.data[-1]
+        index_of_elem_to_remove = self.data_map[val]
+
+        self.data_map[last_elem_in_list] = index_of_elem_to_remove
+        self.data[index_of_elem_to_remove] = last_elem_in_list
+
+        # change the last element in the list to now be the value of the element 
+        # we want to remove
+        self.data[-1] = val
+
+        # remove the last element in the list
+        self.data.pop()
+
+        # remove the element to be removed from the dictionary
+        self.data_map.pop(val)
+        return True
 
     def getRandom(self) -> int:
-        a=random.sample(self.s,k=1)
-        return a[0]
-        """
-        Get a random element from the set.
-        """
-        
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+        # if running outside of leetcode, you need to `import random`.
+        # random.choice will randomly select an element from the list of data.
+        return random.choice(self.data)
