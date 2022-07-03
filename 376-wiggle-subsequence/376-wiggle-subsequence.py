09abf2550c6747891d4915d4cad7841ff2ba9421
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-07-03 10:00:11 +0530

    Time: 43 ms (77.49%), Space: 13.9 MB (77.55%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -18,4 +18,25 @@ class Solution:
                 length += 1
                 up = False
         return length
-            
+    
+    def wiggledp(self,nums):
+        
+        n=len(nums)
+        
+        if n<2:
+            return n
+        
+        up=[0]*n
+        
+        down=[0]*n
+        
+        for i in range(1,n):
+            for j  in range(i):
+                
+                if nums[i]>nums[j]:
+                    up[i]=max(up[i],down[j]+1)
+                
+                elif nums[i]<nums[j]:
+                    down[i]=max(down[i],up[j]+1)
+                    
+        return 1+max(down[-1],up[-1])
