6df4296069f1e49c8028548f307fa04900fd6b5e
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-07-04 09:01:02 +0530

    Time: 991 ms (43.19%), Space: 28 MB (31.57%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -9,6 +9,22 @@ class Solution:
             
             nums[abs(nums[i])]*=-1
             
+#         # Find the intersection point of the two runners.
+#         tortoise = hare = nums[0]
+#         while True:
+#             tortoise = nums[tortoise]
+#             hare = nums[nums[hare]]
+#             if tortoise == hare:
+#                 break
+        
+#         # Find the "entrance" to the cycle.
+#         tortoise = nums[0]
+#         while tortoise != hare:
+#             tortoise = nums[tortoise]
+#             hare = nums[hare]
+        
+#         return hare
+            
             
             
         
