a78ad1db5d5f772ee97883576a71c64a092f54b9
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-07-05 20:24:03 +0530

    Time: 3980 ms, Memory: 30.2 MB - LeetHub

---
+++ sliding-window-maximum/sliding-window-maximum.py
@@ -0,0 +1,49 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        
+        ''' sliding window of size k  
+            there is one maximum element
+            for each window we need to add the current maximum
+            
+            time complexity -O(n)
+            It seems more than O(n) at first look. It can be observed that every element of array is added and removed at most once. So there are total 2n operations
+            
+            space - O(k) deque
+            
+            store indexes
+            '''
+        
+        n=len(nums)
+        res=[]
+        q = deque()
+        
+        for i in range(k):
+            
+            while q  and nums[q[-1]]<= nums[i]:
+                q.pop()
+                
+            q.append(i)
+        
+        print(q)
+            
+        for i in range(k,n):
+            
+            res.append(nums[q[0]])
+            
+            while q and q[0] <= i-k:   # remove out of bound elements
+                
+                q.popleft()
+                
+            while q and nums[q[-1]] <= nums[i]:   # remove useless elements
+                q.pop()
+                
+            q.append(i)
+            
+        
+        res.append(nums[q[0]])
+        
+        return res
+        
+    
+        
+        
