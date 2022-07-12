8009af9a8a39147a4157c88fdf965c57d0706bbc
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-07-12 11:28:25 +0530

    Time: 173 ms (24.63%), Space: 13.9 MB (81.40%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        comb = []
+        
+        results =[]
+        self.backtrack(candidates,comb,target,0,results)
+        
+        return results
+    
+    
+    def backtrack(self,candidates,comb,target,curr,results):
+        
+        if target<0:
+            return
+        if target==0:
+            
+            results.append(list(comb))
+            return 
+            
+        for next_curr in range(curr,len(candidates)):
+            
+                
+            pick = candidates[next_curr]
+            
+            comb.append(pick)
+            
+            self.backtrack(candidates,comb,target-pick,next_curr,results)
+            
+            comb.pop()
