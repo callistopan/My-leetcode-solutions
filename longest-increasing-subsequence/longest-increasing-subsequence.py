119548264f0d11d2add1b9d480e34f5d1aee3209
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-07-15 11:50:44 +0530

    Time: 130 ms, Memory: 14.2 MB - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.py
+++ longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,4 +1,20 @@
 class Solution:
+    ''' first create an array sub for storing our elements
+        we iterate over the array and for each element we need to find the position of this element         in our sub array
+        - use binary search for this 
+        - if position returned is same as the len of sub array then this is a new larger element
+          so we can just append the element to our sub array
+          
+        - else there is a valid point to insert ,so just insert the element into that position
+        
+        -2,3,7,101 of lenght 4
+        
+        10,9,2,5,3,7,101,18
+        
+        sub =[2,3,18,101] 
+        
+        
+        '''
     def lengthOfLIS(self, nums: List[int]) -> int:
         #tails array is always sorted 
         def binarySearch(sub, val):
