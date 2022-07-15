38fa03c85b07879076444f60425566f6c7e7fc0c
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-07-15 11:32:12 +0530

    Time: 540 ms, Memory: 15.1 MB - LeetHub

--- jump-game/jump-game.py
+++ jump-game/jump-game.py
@@ -1,6 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        
+        ''' iterate over nums and update the maximum index we can reach each time
+            whenever we find a index that is greater than the max index then there is no 
+            possible way to reach this point ,so we return false else return true'''
         max_index=0
+        
         for i in range(len(nums)):
             if i >max_index:
                 return False
