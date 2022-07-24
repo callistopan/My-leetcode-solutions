b94f5f8ddfd9f67c412625ab8beeb0378b588817
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-07-24 12:09:09 +0530

    Time: 924 ms (33.67%), Space: 40.9 MB (38.48%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+        q = deque()
+        q.append(root)
+        is_even = True
+        while q:
+            prev = None
+            for _ in range(len(q)):
+                node = q.popleft()
+                if is_even:
+                    if node.val % 2 == 0: return False
+                    if prev and prev.val >= node.val: return False
+                else:
+                    if node.val % 2 == 1: return False
+                    if prev and prev.val <= node.val: return False
+                if node.left: q.append(node.left)
+                if node.right: q.append(node.right)
+                prev = node
+            is_even = not is_even
+        return True
