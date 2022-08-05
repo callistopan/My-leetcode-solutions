92a7853e5b645e1c2be9108d1e780e4c83d537be
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-08-05 21:36:56 +0530

    Time: 796 ms (70.69%), Space: 17.3 MB (85.93%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        
+        n= len(words)
+        
+        bit_mask =[0]*n
+        
+        length =[0]*n
+        
+        for i in range(n):
+            
+            for c in words[i]:
+                
+                bit_mask[i]= bit_mask[i] | (1<<(ord(c)-ord('a')))   # set the curresponding bit of character
+                
+            length[i]= len(words[i])
+            
+        # and all the word bit_masks if it become zero then we are sure that there is no common letters
+        
+        
+        max_len= float('-inf')
+        for i in range(n):
+            
+            for j in range(i+1,n):
+                
+                if bit_mask[i] & bit_mask[j]==0 and length[i]*length[j] > max_len:
+                    
+                    max_len= length[i]*length[j]
+                    
+        return max_len if max_len!= float('-inf') else 0
+            
+            
+        
