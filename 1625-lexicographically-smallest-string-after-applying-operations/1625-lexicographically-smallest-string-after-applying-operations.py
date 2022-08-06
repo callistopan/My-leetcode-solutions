f5be3cc07fb83075a496a09be4369775b6cbd20c
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-08-06 17:50:19 +0530

    Time: 1946 ms (68.29%), Space: 15.9 MB (50.00%) - LeetHub

---
+++ 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.py
@@ -0,0 +1,66 @@
+class Solution:
+    def findLexSmallestString(self, s: str, a: int, b: int) -> str:
+        
+        q = deque()
+        
+        seen = {s}
+        
+        smallest =s
+        
+        q.append(s)
+        
+        
+        while q:
+            
+            curr = q.popleft()
+            
+            if smallest > curr:
+                
+                smallest =curr
+                
+            add_a = list(curr)
+            
+            # add a to the odd positions 
+            for i ,c in enumerate(add_a):
+                
+                if i %2==1:
+                    
+                    add_a[i] =str((int(c)+a)%10)
+                    
+            add_a =''.join(add_a)
+            
+            if add_a not in seen:
+                
+                seen.add(add_a)
+                
+                q.append(add_a)
+             
+            # rotate the array
+            rotate=curr[-b:] + curr[:-b]
+            
+            if rotate not in seen:
+                
+                seen.add(rotate)
+                
+                q.append(rotate)
+                
+        return smallest
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+                    
+            
