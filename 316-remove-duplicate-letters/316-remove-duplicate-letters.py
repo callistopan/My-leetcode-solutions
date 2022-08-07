82caf5df39c4e918a8cd840f330f3b9ec5c3ac4e
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-08-07 14:03:16 +0530

    Time: 64 ms (35.49%), Space: 13.8 MB (80.11%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        
+        rindex = {c: i for i, c in enumerate(s)}
+        print(rindex)
+        result = ''
+        for i, c in enumerate(s):
+            if c not in result:
+                while result and c < result[-1] and i < rindex[result[-1]]:  # we can remove the last element of the result if current character is less than result[-1] and there is an appearance of result[-1] onn right 
+                    result = result[:-1]
+                result += c
+        return result
