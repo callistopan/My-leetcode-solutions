3220f7435f868fddbd4571b8c5c6c8f897faf779
MOHAMMED HASHIM K <80586987+callistopan@users.noreply.github.com>
2022-08-30 07:55:59 +0530

    Time: 111 ms (71.01%), Space: 6.1 MB (55.91%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // the idea is to generate all possible permutations of the given sequence optimally
+    
     int count=0;
     int countArrangement(int n) {
         vector<bool> visited(n+1,0);
