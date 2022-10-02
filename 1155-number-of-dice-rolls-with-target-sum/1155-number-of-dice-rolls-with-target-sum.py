9363f22f152ad946024ad27f388b3edade669630
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2022-10-02 07:37:30 +0530

    Time: 201 ms (93.64%), Space: 14.9 MB (62.25%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numRollsToTarget(self, d: int, f: int, target: int) -> int:
+        if d*f < target: return 0        # Handle special case, it speed things up, but not necessary
+        elif d*f == target: return 1     # Handle special case, it speed things up, but not necessary
+        mod = int(10**9 + 7)
+        dp = [[0] * (target+1) for _ in range(d+1)] 
+        for j in range(1, min(f+1, target+1)):
+            dp[1][j] = 1
+        for i in range(2, d+1): # number of rolls
+            for j in range(1, target+1): # target
+                for k in range(1, f+1):
+                    if j - k >= 0:
+                        dp[i][j] += dp[i-1][j-k]  # 1 less roll and target-this face
+                dp[i][j] %= mod 
+        print(dp)
+        return dp[-1][-1]
