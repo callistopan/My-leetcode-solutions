9524cb1e44b637ecbdf0ce5976fd81679982130f
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2022-10-05 12:26:11 +0530

    Time: 4 ms (81.51%), Space: 8.6 MB (83.70%) - LeetHub

--- 781-rabbits-in-forest/781-rabbits-in-forest.cpp
+++ 781-rabbits-in-forest/781-rabbits-in-forest.cpp
@@ -8,3 +8,21 @@ public:
         return res;
     }
 };
+
+/*
+
+0 0 1 1 1
+
+here 3 rabbits answered as 1 so first rabbit know there is one another rabbit of same color => let 1 1 be red=2
+other rabbit know there is one more of same colour let it be 1 =2
+0 0 =>2
+generally there is 3 rabbits have said 1 
+so there is like 3/2+1 groups here x=1 x+1=2 (generally)
+if it is 3 3 3 => 3/4+1 =>1 group 
+33333 5/4+1=2 groups
+
+
+
+
+
+*/
