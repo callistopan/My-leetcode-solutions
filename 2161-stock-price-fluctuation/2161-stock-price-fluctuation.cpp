68ff7ea1b0aa1872347a7c326ee1b43ef2e09e77
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2023-06-19 11:22:20 +0530

    Time: 562 ms (42.04%), Space: 168.7 MB (23.99%) - LeetHub

---
+++ 2161-stock-price-fluctuation/2161-stock-price-fluctuation.cpp
@@ -0,0 +1,37 @@
+class StockPrice {
+public:
+    map<int,int> rec; // key= time stamp , val = price 
+    multiset<int> count; // store prices
+    StockPrice() {
+        
+    }
+    
+    void update(int timestamp, int price) {
+        if (rec.find(timestamp) != rec.end()){
+            count.erase(count.find(rec[timestamp]));  // remove all prices from multiset
+        }
+        rec[timestamp] =price; // set new price
+        count.insert(price); // insert price to multiset
+    }
+    
+    int current() {
+        return rec.rbegin()->second; // price for last time stamp
+    }
+    
+    int maximum() {
+        return *count.rbegin();  // end of multiset
+    }
+    
+    int minimum() {
+        return *count.begin();   // beginning of multiset
+    }
+};
+
+/**
+ * Your StockPrice object will be instantiated and called as such:
+ * StockPrice* obj = new StockPrice();
+ * obj->update(timestamp,price);
+ * int param_2 = obj->current();
+ * int param_3 = obj->maximum();
+ * int param_4 = obj->minimum();
+ */
