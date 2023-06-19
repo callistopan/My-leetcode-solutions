2c586f4f21739c1121414e96d8cd06a850580f64
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2023-06-19 21:35:26 +0530

    Time: 44 ms (71.3%), Space: 16.3 MB (79.89%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.py
+++ 1304-longest-happy-string/1304-longest-happy-string.py
@@ -4,16 +4,18 @@ class Solution:
         counts = [(a, 'a'), (b, 'b'), (c, 'c')]
 
         while True:
-            counts.sort(reverse=True)
-            if counts[0][0] == 0:
+            counts.sort(reverse=True) # higest to lowest eg: a=5 b=3 c=2
+            if counts[0][0] == 0: # there is nothing left
                 break
-            if len(result) >= 2 and result[-1] == counts[0][1] and result[-2] == counts[0][1]:
-                if counts[1][0] == 0:
+            # violates the 3 substring proprerty then choose the next highest char
+            if len(result) >= 2 and result[-1] == counts[0][1] and result[-2] == counts[0][1]: 
+                if counts[1][0] == 0: # no next highest char
                     break
-                result.append(counts[1][1])
-                counts[1] = (counts[1][0] - 1, counts[1][1])
+                result.append(counts[1][1]) 
+                counts[1] = (counts[1][0] - 1, counts[1][1]) # update the count of second highest
+            # no violation of any property just append the highest freq character to result
             else:
-                result.append(counts[0][1])
-                counts[0] = (counts[0][0] - 1, counts[0][1])
+                result.append(counts[0][1])  
+                counts[0] = (counts[0][0] - 1, counts[0][1]) 
 
         return ''.join(result)
