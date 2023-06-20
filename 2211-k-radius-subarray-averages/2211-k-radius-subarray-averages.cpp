b9335dbaf19e9f8cb4bbd8415dbbc8fde85dbe75
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2023-06-20 11:26:15 +0530

    Time: 282 ms (15.37%), Space: 130 MB (59.59%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.cpp
@@ -1,29 +1,36 @@
-class Solution
-{
-    public:
-        vector<int> getAverages(vector<int> &nums, int k)
-        {
-            int n = nums.size();
-            vector<int> res(n, -1);
-           
-           	// prefix
-            vector<long long> prefix(n, 0);
-            for (int i = 1; i < n; i++)
-            {
-                prefix[i] = prefix[i - 1] + nums[i - 1];
-            }
-
-            for (int i = k; i < n - k; i++)
-            {
-                res[i] = (nums[i + k] + prefix[i + k] - prefix[i - k]) / (2 *k + 1);
-            }
-
-            return res;
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& nums, int k) {
+        // When a single element is considered then its averafge will be the number itself only.
+        if (k == 0) {
+            return nums;
         }
-};
 
-// valid 2k + 1 elements
-// start from the index k up to n-k
-// calculate the prefix and suffix sum of array 
-//      ......i-k....i......i+k  sum = prefix[i+k] + nums[i+k] - prefix[i-k]
-// average = sum/2k+1
+        int n = nums.size();
+        vector<int> averages(n, -1);
+
+        // Any index will not have 'k' elements in it's left and right.
+        if (2 * k + 1 > n) {
+            return averages;
+        }
+
+        // First get the sum of first window of the 'nums' arrray.
+        long long windowSum = 0;
+        for (int i = 0; i < (2 * k + 1); ++i) {
+            windowSum += nums[i];
+        }
+        averages[k] = windowSum / (2 * k + 1);
+
+        // Iterate on rest indices which have atlest 'k' elements 
+        // on its left and right sides.
+        for (int i = (2 * k + 1); i < n; ++i) {
+            // We remove the discarded element and add the new element to get current window sum.
+            // 'i' is the index of new inserted element, and
+            // 'i - (window size)' is the index of the last removed element.
+            windowSum = windowSum - nums[i - (2 * k + 1)] + nums[i];
+            averages[i - k] = windowSum / (2 * k + 1);
+        }
+
+        return averages;
+    }
+};
