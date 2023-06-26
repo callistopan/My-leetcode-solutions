35816971635144add0fc9f75fb45e844bf24a4fe
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2023-06-26 07:49:51 +0530

    Time: 196 ms (82.48%), Space: 72.8 MB (82.21%) - LeetHub

---
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        int n  = costs.size();
+        priority_queue<int, vector<int>, greater<int>> LeftHeap;
+        priority_queue<int, vector<int>, greater<int>> RightHeap;
+        // push elements to left heap
+        for (int i = 0;i < candidates ; i++){
+            LeftHeap.push(costs[i]);
+        }
+        // push elementws to right heap
+        for (int i = n-1; i>= max(candidates,n-candidates);i--){
+            RightHeap.push(costs[i]);
+        }
+
+        int l = candidates-1;
+        int r = max(candidates,n-candidates);
+        long long cost = 0;
+
+        while (k--){
+
+            if (!LeftHeap.empty() && !RightHeap.empty()){
+                // left heap has the smallest cost
+                if (LeftHeap.top() <= RightHeap.top()){
+                    cost += LeftHeap.top();
+                    LeftHeap.pop();
+                    if (l+1<r && l+1<n){
+                        l ++;
+                        LeftHeap.push(costs[l]);
+                    }
+                }
+                else{
+                    cost += RightHeap.top();
+                    RightHeap.pop();
+                    if (r-1>l && r-1 >=0){
+                        r --;
+                        RightHeap.push(costs[r]);
+                    }
+                }
+            }
+            else if (RightHeap.empty()){
+                cost += LeftHeap.top();
+                LeftHeap.pop();
+            }
+            else{
+                cost += RightHeap.top();
+                RightHeap.pop();
+            }
+
+        }
+        return cost;
+    }
+};
