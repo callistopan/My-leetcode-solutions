2991149d30356af125df7ebdd9e3b5f2815f4693
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2023-06-30 09:05:20 +0530

    Time: 1993 ms (69.02%), Space: 25.1 MB (90.76%) - LeetHub

--- 2101-last-day-where-you-can-still-cross/2101-last-day-where-you-can-still-cross.py
+++ 2101-last-day-where-you-can-still-cross/2101-last-day-where-you-can-still-cross.py
@@ -1,37 +1,43 @@
-class Solution:
-    def canCross(self, row, col, cells, day):
-        grid = [[0] * col for _ in range(row)]
-        queue = collections.deque()
-        
-        for r, c in cells[:day]:
-            grid[r - 1][c - 1] = 1
-            
-        for i in range(col):
-            if not grid[0][i]:
-                queue.append((0, i))
-                grid[0][i] = -1
+class DSU:
+    def __init__(self, n):
+        self.root = list(range(n)) # 0,1,2,3.....,row*col,row*col+1
+        self.size = [1] * n   # initially one element (the roots)
 
-        while queue:
-            r, c = queue.popleft()
-            if r == row - 1:
-                return True
-            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                new_row, new_col = r + dr, c + dc
-                if 0 <= new_row < row and 0 <= new_col < col and grid[new_row][new_col] == 0:
-                    grid[new_row][new_col] = -1
-                    queue.append((new_row, new_col))
-                    
-        return False
-    
-    
-    def latestDayToCross(self, row: int, col: int, cells: List[List[int]]) -> int:
-        left, right = 1, row * col
+    def find(self, x):
+        if self.root[x] != x:
+            self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x == root_y:
+            return
         
-        while left < right:
-            mid = right - (right - left) // 2
-            if self.canCross(row, col, cells, mid):
-                left = mid
-            else:
-                right = mid - 1
-                
-        return left
+        if self.size[root_x] > self.size[root_y]:
+            root_x, root_y = root_y, root_x
+        self.root[root_x] = root_y
+        self.size[root_y] = self.size[root_x]
+
+class Solution:
+    def latestDayToCross(self, row: int, col: int, cells: List[List[int]]) -> int:
+        dsu = DSU(row * col + 2)
+        grid = [[1] * col for _ in range(row)]
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        for i in range(len(cells) - 1, -1, -1):
+            r, c = cells[i][0] - 1, cells[i][1] - 1
+            grid[r][c] = 0
+            index_1 = r * col + c + 1
+            for dr, dc in directions:
+                new_r, new_c = r + dr, c + dc
+                index_2 = new_r * col + new_c + 1
+                if 0 <= new_r < row and 0 <= new_c < col and grid[new_r][new_c] == 0:
+                    dsu.union(index_1, index_2)
+                    
+            if r == 0:
+                dsu.union(0, index_1)
+            if r == row - 1:
+                dsu.union(row * col + 1, index_1)
+            if dsu.find(0) == dsu.find(row * col + 1):
+                return i
