304fcac83da43dd249c696e6d098f170cb59c77b
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2023-07-02 17:42:00 +0530

    Time: 108 ms (67.65%), Space: 8.7 MB (92.16%) - LeetHub

--- 1723-maximum-number-of-achievable-transfer-requests/1723-maximum-number-of-achievable-transfer-requests.cpp
+++ 1723-maximum-number-of-achievable-transfer-requests/1723-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    int maximumRequests(int n, vector<vector<int>>& requests) {
-        int answer = 0; 
-        
-        for (int mask = 0; mask < (1 << requests.size()); mask++) {
-            vector<int> indegree(n, 0);
-            int pos = requests.size() - 1;
-            // Number of set bits representing the requests we will consider.
-            int bitCount = __builtin_popcount(mask);
-            
-            // If the request count we're going to consider is less than the maximum request 
-            // We have considered without violating the constraints; then we can return it cannot be the answer.
-            if (bitCount <= answer) {
-                continue;
-            }
-            
-            // For all the 1's in the number, update the array indegree for the building it involves.
-            for (int curr = mask; curr > 0; curr >>= 1, pos--) {
-                if (curr & 1) {
-                    indegree[requests[pos][0]]--;
-                    indegree[requests[pos][1]]++;
-                }
-            }
-            
-            int flag = 1;
-            // Check if it doesn;t violates the constraints
+    int answer = 0;
+
+    void maxRequest(vector<vector<int>>& requests, vector<int>& indegree, int n, int index, int count) {
+        if (index == requests.size()) {
+            // Check if all buildings have an in-degree of 0.
             for (int i = 0; i < n; i++) {
                 if (indegree[i]) {
-                    flag = 0;
-                    break;
+                    return;
                 }
             }
             
-            if (flag)  {
-                answer = bitCount;
-            }
+            answer = max(answer, count);
+            return;
         }
+        
+        // Consider this request, increment and decrement for the buildings involved.
+        indegree[requests[index][0]]--;
+        indegree[requests[index][1]]++;
+        // Move on to the next request and also increment the count of requests.
+        maxRequest(requests, indegree, n, index + 1, count + 1);
+        // Backtrack to the previous values to move back to the original state before the second recursion.
+        indegree[requests[index][0]]++;
+        indegree[requests[index][1]]--;
+        
+        // Ignore this request and move on to the next request without incrementing the count.
+        maxRequest(requests, indegree, n, index + 1, count);
+    }
+    
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        vector<int> indegree(n, 0);
+        maxRequest(requests, indegree, n, 0, 0);
         
         return answer;
     }
