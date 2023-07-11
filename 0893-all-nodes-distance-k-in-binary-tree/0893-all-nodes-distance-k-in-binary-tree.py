0fce3ad6dea8a3a1b44ccc619a19d934ae4dd71c
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2023-07-11 23:08:37 +0530

    Time: 55 ms (54.51%), Space: 16.9 MB (35.25%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.py
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.py
@@ -7,16 +7,16 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        graph = defaultdict(list)
+        graph = defaultdict(list) # graph
 
         def build_graph(current,parent):
 
-            if current and parent:
+            if current and parent:  # current node has a parent
                 graph[current.val].append(parent.val)
                 graph[parent.val].append(current.val)
-            if current.left:
+            if current.left: # has a left child , recurse for that node
                 build_graph(current.left,current)
-            if current.right:
+            if current.right: # has a right chilid, recurse for that node
                 build_graph(current.right,current)
 
 
@@ -24,17 +24,17 @@ class Solution:
         build_graph(root,None)
 
         # dfs
-        seen = set()
+        seen = set() # for storing visited nodes 
         seen.add(target.val)
         answer = []
 
         def dfs(current,distance):
 
-            if distance == k:
+            if distance == k: # distance k from target
                 answer.append(current)
                 return 
 
-            for neighbor in graph[current]:
+            for neighbor in graph[current]: # visit all neighbor node
                 if neighbor not in seen:
                     seen.add(neighbor)
                     dfs(neighbor,distance + 1)
