fdef2e4b075450fb7bacc77cc6fca6391199017d
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2023-07-29 11:31:36 +0530

    Time: 46 ms (88.12%), Space: 16.7 MB (19.64%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -7,10 +7,10 @@ class Solution:
                 return memo[(left,right)]
             if left == right:
                 return nums[left]
-            score_by_left = nums[left] - maxDiff(left+1 , right)
+            score_by_left = nums[left] - maxDiff(left+1 , right) # other player plays optimally
             score_by_right = nums[right] - maxDiff(left, right-1)
 
-            memo[(left,right)] = max(score_by_left, score_by_right)
+            memo[(left,right)] = max(score_by_left, score_by_right) # maximum of these two possibilities
             return memo[(left,right)]
 
         return maxDiff(0,n-1) >=0
