372203c91281fb80022356ca2f32aee2499fa600
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2023-08-29 22:45:06 +0530

    Time: 70 ms (66.67%), Space: 42.3 MB (33.33%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cs
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cs
@@ -1,27 +1,18 @@
 public class Solution {
     public int BestClosingTime(string customers) {
-        // start with closing at hour 0 , the penalty is all 'Y' in closed hours
-        int curPenalty = customers.Count(c => c=='Y');
+        int curPenalty = customers.Count(c => c == 'Y');
         int minPenalty = curPenalty;
-
         int earliestHour = 0;
 
-        for (int i = 0 ; i < customers.Length; i++){
+        for (int i = 0; i < customers.Length; i++) {
             char ch = customers[i];
-            if (ch == 'Y'){
-                curPenalty --;
-            }
-            else{
-                curPenalty ++;
-            }
+            curPenalty += (ch == 'Y') ? -1 : 1;
 
-            if (curPenalty < minPenalty){
-                earliestHour  = i +1;
+            if (curPenalty < minPenalty) {
+                earliestHour = i + 1;
                 minPenalty = curPenalty;
             }
         }
         return earliestHour;
-
-        
     }
 }
