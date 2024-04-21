ba4fe8c8e82ca0595ab075d3502990bd92a2fe98
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2024-04-21 10:36:26 +0530

    Time: 0 ms (100.00%), Space: 5.3 MB (83.33%) - LeetHub

---
+++ 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.c
@@ -0,0 +1,9 @@
+#include <math.h>
+int minTimeToType(char* word) {
+    int n = strlen(word);
+    int res = n;
+    for(int i = 0;i < n - 1;i++){
+        res += fmin(abs(word[i] - word[i+1]) , 26 - abs(word[i] - word[i+1]));
+    }
+    return res + fmin(abs((int) 'a' - word[0]), 26 - abs((int) 'a' - word[0]));
+}
