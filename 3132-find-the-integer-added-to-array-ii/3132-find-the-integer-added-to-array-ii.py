c71a141f63ea7f4bcf7c3753a375152530c4b234
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2024-04-28 09:37:53 +0530

    Time: 816 ms (100.00%), Space: 16.5 MB (100.00%) - LeetHub

---
+++ 3132-find-the-integer-added-to-array-ii/3132-find-the-integer-added-to-array-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumAddedInteger(self, nums1: List[int], nums2: List[int]) -> int:
+        
+        count1 , count2  = Counter(nums1), Counter(nums2)
+        for x in range(-1005, 1005):
+            is_valid = True
+            for y in nums2:
+                if count1[y-x] < count2[y]:
+                    is_valid = False
+            if is_valid:
+                return x
+                
+            
