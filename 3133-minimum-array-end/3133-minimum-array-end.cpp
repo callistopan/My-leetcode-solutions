696e76abc2efef527a8730f1b804ed61f4496947
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2024-04-28 14:18:13 +0530

    Time: 1726 ms (10.00%), Space: 7.9 MB (70.00%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.cpp
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -7,3 +7,20 @@ public:
         return a;
     }
 };
+
+/* Example 
+
+n = 3 x = 4
+start with x = 4 , for minimum
+100
+101
+110
+
+n = 5 x = 9
+start with x = 9 , for minimum
+1001
+1011           
+1101
+1111
+11001
+*/
