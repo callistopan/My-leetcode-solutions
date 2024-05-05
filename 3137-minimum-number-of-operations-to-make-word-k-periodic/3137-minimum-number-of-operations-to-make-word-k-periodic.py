4a4b28f1dcd8fcca572c125e5f79ee6e3ed83ae5
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2024-05-05 09:36:21 +0530

    Time: 139 ms (50.00%), Space: 18.8 MB (100.00%) - LeetHub

---
+++ 3137-minimum-number-of-operations-to-make-word-k-periodic/3137-minimum-number-of-operations-to-make-word-k-periodic.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimumOperationsToMakeKPeriodic(self, word: str, k: int) -> int:
+        cnt = Counter()
+        for i in range(0, len(word), k):
+            cnt[word[i:i+k]] += 1
+        return len(word) // k - max(cnt.values())
+        
