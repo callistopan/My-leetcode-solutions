8d0bfa07cb42e717a3699869369b3b51cc623671
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2024-05-08 23:27:07 +0530

    Time: 1188 ms (17.66%), Space: 17.6 MB (25.04%) - LeetHub

---
+++ 3138-minimum-length-of-anagram-concatenation/3138-minimum-length-of-anagram-concatenation.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minAnagramLength(self, s: str) -> int:
+        n = len(s)
+        for i in range(1,n+1):
+            if n % i == 0 and self.ok(s, i):
+                return i
+        return n
+    def ok(self,s,k):
+        n = len(s)
+        cnt1 = Counter(s[:k])
+        
+        for i in range(k,n,k):
+            cnt2 = Counter(s[i: i +k])
+            if cnt1 != cnt2:
+                return False
+        return True
