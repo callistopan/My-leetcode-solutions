ac7a200daf2d32c652183bb7664589a747f38d07
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2024-06-01 08:30:51 +0530

    Time: 4 ms (33.69%), Space: 5.4 MB (60.20%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.c
+++ 3110-score-of-a-string/3110-score-of-a-string.c
@@ -2,7 +2,7 @@ int scoreOfString(char* s) {
     int n = strlen(s);
     int sum = 0;
     for (int i = 0; i < n - 1; i++){
-        sum += abs((int)s[i] - (int)s[i+1]);
+        sum += abs(s[i] - s[i+1]);
     }
     return sum;
 }
