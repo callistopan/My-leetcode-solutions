7c5b3b45ae9b750c006f18cfe91de379120f2b41
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2024-07-16 08:43:57 +0530

    Time: 152 ms (66.21%), Space: 116.2 MB (89.60%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+        string s_p, d_p; // start path and destination path
+        find(root, startValue, s_p);
+        find(root, destValue, d_p);
+
+        while (!s_p.empty()  && !d_p.empty() && s_p.back() == d_p.back()){
+            // remove prefixes
+            s_p.pop_back();
+            d_p.pop_back();
+        }
+        return string(s_p.size(),'U') + string(rbegin(d_p),rend(d_p)); 
+    }
+
+    // find the shortest path string to val in the binary tree
+    bool find(TreeNode* n, int val, string& path ){
+        if (n->val == val){
+            return true;
+        }
+        if (n->left && find(n->left, val, path)){
+            path.push_back('L');
+        }
+        else if (n->right && find(n->right,val, path)){
+            path.push_back('R');
+        }
+        return !path.empty();
+    }
+};
+
+
+
+
+
+
+
