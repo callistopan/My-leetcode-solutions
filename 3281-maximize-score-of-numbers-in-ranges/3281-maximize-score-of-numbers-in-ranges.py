9e08f81010015835eec10ae5146f7dccfd8c8162
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2024-09-08 11:55:14 +0530

    Time: 2077 ms (100.00%), Space: 31.1 MB (100.00%) - LeetHub

---
+++ 3281-maximize-score-of-numbers-in-ranges/3281-maximize-score-of-numbers-in-ranges.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxPossibleScore(self, start: List[int], d: int) -> int:
+        # 6, 7, 8
+        # 0,1,2
+        # 3,4,54
+        start.sort()
+        
+        def fn(mid): 
+            x = -inf
+            for s in start: 
+                x += mid
+                if x > s+d: return False 
+                x = max(x, s)
+            return True 
+        
+        lo, hi = 0, 2_000_000_000
+        while lo < hi: 
+            mid = lo + hi + 1 >> 1
+            if fn(mid): lo = mid
+            else: hi = mid-1
+        return lo
