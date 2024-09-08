cf614962ac5398c089c73bdf59c90ea896e74204
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2024-09-08 14:59:02 +0530

    Time: 26 ms (9.82%), Space: 12 MB (64.29%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
@@ -1,17 +1,36 @@
 class Solution(object):
     def splitListToParts(self, root, k):
+        # Step 1: Count the total number of nodes in the linked list
         cur = root
-        for N in xrange(1001):
-            if not cur: break
+        N = 0
+        while cur:
+            N += 1
             cur = cur.next
+        
+        # Step 2: Determine the size of each part (width) and how many parts will have an extra node (remainder)
         width, remainder = divmod(N, k)
-
+        
+        # Step 3: Split the list into parts
         ans = []
         cur = root
-        for i in xrange(k):
-            head = write = ListNode(None)
-            for j in xrange(width + (i < remainder)):
-                write.next = write = ListNode(cur.val)
-                if cur: cur = cur.next
-            ans.append(head.next)
+        for i in range(k):
+            # Start a new part
+            head = cur
+            # Calculate the correct size for the current part
+            part_size = width + (1 if i < remainder else 0)
+            
+            # Move the pointer to the end of the current part
+            for j in range(part_size - 1):
+                if cur:
+                    cur = cur.next
+            
+            # If there's a current node, disconnect the end of the current part and move to the next part
+            if cur:
+                next_part = cur.next
+                cur.next = None
+                cur = next_part
+            
+            # Add the current part to the result
+            ans.append(head)
+        
         return ans
