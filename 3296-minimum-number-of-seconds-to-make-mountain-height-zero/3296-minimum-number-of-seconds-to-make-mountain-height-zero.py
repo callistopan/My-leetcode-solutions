e4801f781c2c18fd6a9eda5f8d706a73fc183ba3
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2024-09-22 22:30:54 +0530

    Time: 2726 ms (100.00%), Space: 17.8 MB (100.00%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.py
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.py
@@ -3,25 +3,25 @@ class Solution:
         def helper(m):
             total = 0
             for wt in workerTimes:
-                left_x = 0
-                right_x = mountainHeight
-                while left_x < right_x:
-                    mid = (left_x + right_x + 1) // 2
-                    cost = wt * mid * (mid + 1) // 2
-                    if cost <= m:
-                        left_x = mid
+                l_height = 0
+                r_height = mountainHeight
+                while l_height < r_height:
+                    mid_height = (l_height + r_height + 1) // 2
+                    time = wt * mid_height * (mid_height + 1) // 2
+                    if time <= m:
+                        l_height = mid_height
                     else:
-                        right_x = mid - 1
-                total += left_x
+                        r_height = mid_height - 1
+                total += l_height
                 if total >= mountainHeight:
                     return True
             return total >= mountainHeight
 
-        left, right = 0, max(workerTimes) * mountainHeight * (mountainHeight +1) //2
-        while left < right:
-            mid = (left + right) // 2
-            if helper(mid):
-                right = mid
+        l_time, r_time = 0, max(workerTimes) * mountainHeight * (mountainHeight +1) //2
+        while l_time < r_time:
+            mid_time = (l_time + r_time) // 2
+            if helper(mid_time):
+                r_time = mid_time
             else:
-                left = mid + 1
-        return left
+                l_time = mid_time + 1
+        return r_time
