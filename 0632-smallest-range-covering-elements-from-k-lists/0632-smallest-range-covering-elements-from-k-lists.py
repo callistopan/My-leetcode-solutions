179de07ec28966d07c11f6d1be8b4396b2c4d4e6
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2024-10-13 14:59:15 +0530

    Time: 198 ms (43.12%), Space: 23.2 MB (37.22%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
@@ -1,35 +1,25 @@
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
-        merged = []
-
-        # Merge all lists with their list index
-        for i in range(len(nums)):
-            for num in nums[i]:
-                merged.append((num, i))
-
-        # Sort the merged list
-        merged.sort()
-
-        # Two pointers to track the smallest range
-        freq = defaultdict(int)
-        left, count = 0, 0
-        range_start, range_end = 0, float("inf")
-
-        for right in range(len(merged)):
-            freq[merged[right][1]] += 1
-            if freq[merged[right][1]] == 1:
-                count += 1
-
-            # When all lists are represented, try to shrink the window
-            while count == len(nums):
-                cur_range = merged[right][0] - merged[left][0]
-                if cur_range < range_end - range_start:
-                    range_start = merged[left][0]
-                    range_end = merged[right][0]
-
-                freq[merged[left][1]] -= 1
-                if freq[merged[left][1]] == 0:
-                    count -= 1
-                left += 1
-
+        pq = []
+        max_val = -inf
+        range_start = 0
+        range_end = inf
+        n = len(nums)
+        
+        for i in range(n):
+            heapq.heappush(pq, (nums[i][0],i,0))
+            max_val = max(max_val, nums[i][0])
+            
+        while len(pq) == len(nums):
+            min_val , row, col = heapq.heappop(pq)
+            
+            if max_val - min_val < range_end - range_start:
+                range_start = min_val
+                range_end = max_val
+                
+            if col + 1 < len(nums[row]):
+                next_val = nums[row][col + 1]
+                heapq.heappush(pq, (next_val, row, col + 1))
+                max_val= max(max_val, next_val)
+                
         return [range_start, range_end]
