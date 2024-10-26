d1387e03fdd2caeb43b375a11064e9beed1add6d
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2024-10-26 17:35:35 +0530

    Time: 259 ms (96.41%), Space: 59.8 MB (58.97%) - LeetHub

--- 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.py
+++ 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.py
@@ -2,46 +2,37 @@ class Solution:
     def treeQueries(
         self, root: Optional[TreeNode], queries: List[int]
     ) -> List[int]:
-        max_height_after_removal = [0] * 100001
-        self.current_max_height = 0
-
-        def _traverse_left_to_right(node, current_height):
+        Depth, Height = defaultdict(int), defaultdict(int)
+        
+        # Pre-compute Depths and Heights
+        def dfs(node, depth):
             if not node:
-                return
-
-            # Store the maximum height if this node were removed
-            max_height_after_removal[node.val] = self.current_max_height
-
-            # Update the current maximum height
-            self.current_max_height = max(
-                self.current_max_height, current_height
-            )
-
-            # Traverse left subtree first, then right
-            _traverse_left_to_right(node.left, current_height + 1)
-            _traverse_left_to_right(node.right, current_height + 1)
-
-        def _traverse_right_to_left(node, current_height):
-            if not node:
-                return
-
-            # Update the maximum height if this node were removed
-            max_height_after_removal[node.val] = max(
-                max_height_after_removal[node.val], self.current_max_height
-            )
-
-            # Update the current maximum height
-            self.current_max_height = max(
-                current_height, self.current_max_height
-            )
-
-            # Traverse right subtree first, then left
-            _traverse_right_to_left(node.right, current_height + 1)
-            _traverse_right_to_left(node.left, current_height + 1)
-
-        _traverse_left_to_right(root, 0)
-        self.current_max_height = 0  # Reset for the second traversal
-        _traverse_right_to_left(root, 0)
-
-        # Process queries and build the result list
-        return [max_height_after_removal[q] for q in queries]
+                return -1
+            Depth[node.val] = depth
+            cur = max(dfs(node.left, depth + 1), dfs(node.right, depth + 1)) + 1
+            Height[node.val] = cur
+            return cur
+        dfs(root, 0)
+        
+        # Group nodes according to their depths
+        cousins = defaultdict(list)
+        
+        for val, depth in Depth.items():
+            cousins[depth].append((-Height[val], val))
+            cousins[depth].sort()
+            if len(cousins[depth]) > 2:
+                cousins[depth].pop()
+                
+        ans = []
+        for q in queries:
+            depth = Depth[q]
+            if len(cousins[depth]) == 1: # no cousins
+                ans.append(depth - 1)
+            elif cousins[depth][0][1] == q: # q having the largest height
+                ans.append(-cousins[depth][1][0] + depth) # return the second largest
+            else:
+                ans.append(-cousins[depth][0][0] + depth) # return the largest height
+        return ans
+    
+#https://leetcode.com/problems/height-of-binary-tree-after-subtree-removal-queries/discuss/2757990/Python-3-Explanation-with-pictures-DFS 
+        
