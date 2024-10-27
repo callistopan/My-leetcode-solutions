aaa578ef03759a9a0a19ebd1ad44a6941074e13b
MOHAMMED HASHIM K <80586987+mohammed-hashim-k@users.noreply.github.com>
2024-10-27 10:13:31 +0530

    Time: 2058 ms (100.00%), Space: 60.7 MB (100.00%) - LeetHub

--- 3331-find-subtree-sizes-after-changes/3331-find-subtree-sizes-after-changes.py
+++ 3331-find-subtree-sizes-after-changes/3331-find-subtree-sizes-after-changes.py
@@ -1,12 +1,14 @@
 class Solution:
     def findSubtreeSizes(self, parent: List[int], s: str) -> List[int]:
         n = len(parent)
+        # build the graph
         graph = defaultdict(list)
         for i in range(1, n):
             graph[parent[i]].append(i)
         
         new_parent = parent.copy()
         
+        # change the parent if if the character match
         for node in range(1, n):
             curr = parent[node]
             while curr != -1:
@@ -15,10 +17,12 @@ class Solution:
                     break
                 curr = parent[curr]
         
+        # build the new graph
         new_graph = defaultdict(list)
         for i in range(1, n):
             new_graph[new_parent[i]].append(i)
         
+        # count the nodes
         def dfs(node: int) -> int:
             size = 1
             for child in new_graph[node]:
